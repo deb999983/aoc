52ddc3d60ec474c3dee0c80731062ba931342f13
GitLab <pmdi.dev@dnb.no>
2023-12-02 16:18:26 +0100

    Day02_part2 attempt1

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,8 +1,15 @@
+import kotlin.math.max
+
 data class Game(val index: Int, val sets: List<Map<String, Int>>) {
     fun isValid(): Boolean {
         return sets.map { set ->
             set.getOrDefault("red", 0) <= 12 && set.getOrDefault("green", 0) <= 13 && set.getOrDefault("blue", 0) <= 14
         }.reduce { acc, b -> acc && b }
+    }
+    fun requiredCubes(): Map<String, Int> {
+        return sets.reduce { acc, s ->
+            listOf("red", "green", "blue").map { it to max(acc.getOrDefault(it, 0), s.getOrDefault(it, 0)) }.toMap()
+        }
     }
     companion object {
         fun getGameListFromInput(input: List<String>): List<Game> {
@@ -27,15 +34,15 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return Game.getGameListFromInput(input).map { it.requiredCubes().values.reduce { acc, n -> acc * n } }.sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day02_test")
     check(part1(testInput) == 8)
-//    check(part2(testInput) == 317)
+    check(part2(testInput) == 2286)
 
     val input = readInput("Day02")
     part1(input).println()
-//    part2(input).println()
+    part2(input).println()
 }
